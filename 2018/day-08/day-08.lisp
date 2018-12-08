a3dc33eca97e28c3dbe74bb360d9d668ed74e1ef
Marco Heisig <marco.heisig@fau.de>
2018-12-08 14:35:18 +0100

    Finish day 8 exercises.

--- 2018/day-08/day-08.lisp
+++ 2018/day-08/day-08.lisp
@@ -3,3 +3,37 @@
 
 (in-package :adventofcode-2018-day-8)
 
+(defclass node ()
+  ((%children :initarg :children :reader children)
+   (%metadata :initarg :metadata :reader metadata)))
+
+(defun read-node (stream)
+  (let ((n-children (read stream))
+        (n-metadata (read stream)))
+    (make-instance 'node
+      :children (loop repeat n-children collect (read-node stream))
+      :metadata (loop repeat n-metadata collect (read stream)))))
+
+(defun read-tree-from-file (file)
+  (with-open-file (stream file :direction :input)
+    (read-node stream)))
+
+(defun metadata-sum (node)
+  (+ (reduce #'+ (metadata node))
+     (reduce #'+ (children node) :key #'metadata-sum)))
+
+(defun solve-exercise-1 ()
+  (metadata-sum (read-tree-from-file "input")))
+
+(defun node-value (node)
+  (if (null (children node))
+      (reduce #'+ (metadata node))
+      (loop for index in (metadata node)
+            sum
+            (let ((child (nth (1- index) (children node))))
+              (if (not child)
+                  0
+                  (node-value child))))))
+
+(defun solve-exercise-2 ()
+  (node-value (read-tree-from-file "input")))
